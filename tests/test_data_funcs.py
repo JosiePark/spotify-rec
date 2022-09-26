6a18431725fdbd1476c8995db0516bd1e63e6c08
JosiePark <josiepark92@hotmail.co.uk>
2022-09-26 20:49:34 +0100

    settings up github actions

--- tests/test_data_funcs.py
+++ tests/test_data_funcs.py
@@ -8,9 +8,19 @@ def test_extract_artist_from_col():
         {"name": "c", "type" : "artist", "info" : "c1"}
     ]
 
-    print(artist for artist in artist_info)
-
     artists = spotifyRec.data_funcs.extract_artist_from_col(artist_info)
 
     assert artists[0] == "a"
     assert artists[1] == "c"
+
+def test_extract_id_from_col():
+    
+    artist_info = [
+        {"name": "a", "type" : "artist", "id" :"a1"},
+        {"name": "b", "type" : "not_artist", "id" : "b1"},
+        {"name": "c", "type" : "artist", "id" : "c1"}
+    ] 
+    artists = spotifyRec.data_funcs.extract_artist_id_from_col(artist_info)
+
+    assert artists[0] == "a1"
+    assert artists[1] == "c1"  
